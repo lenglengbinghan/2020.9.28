f7406c66fc60b86f6c5b9d23dd44e87ebe9b34a1
lenglengbinghan <1109875584@qq.com>
2020-09-28 16:44:45 +0800

    first commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using XLua;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start(){
+        LuaEnv luaEnv = new LuaEnv();
+        luaEnv.DoString("print('this is my hello world')");
+        Debug.Log("Though coding is very hard for me,I will try my best.");
+    }
+
+}
